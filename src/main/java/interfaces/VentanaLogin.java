39e9dc95867aae9283204ee65a6811cfc453e583
Alan Laguna <alanlaguna2020@gmail.com>
2025-12-29 17:13:11 -0500

    cambio de mensaje para credenciales incorrectas

--- src/main/java/interfaces/VentanaLogin.java
+++ src/main/java/interfaces/VentanaLogin.java
@@ -36,7 +36,7 @@ public class VentanaLogin extends JFrame {
                 new FormularioVuelos().setVisible(true); // Abre la app principal
                 this.dispose(); // Cierra el login
             } else {
-                JOptionPane.showMessageDialog(this, "Credenciales incorrectas");
+                JOptionPane.showMessageDialog(this, "Nombre de usuario o contraseña incorrectas");
             }
         });
 
